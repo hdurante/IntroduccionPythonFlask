9d973acd72e22cf026beab6e90b7afd601732968
Hector Manuel Durante Nuñez <hdurante@italika.com.mx>
2023-03-21 12:11:11 -0600

    Se agrega deshabilitar CORS para ejercicios

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from flask import Flask,jsonify,request
+from flask_cors import CORS
 
 app=Flask(__name__)
+CORS(app)
 
 idEntity=2
 People = [{"Id":1,"Nombre": "Fulano", "ApellidoPaterno": "De", "ApellidoMaterno": "Tal"},
